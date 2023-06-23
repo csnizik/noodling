71efde94cdec453d9a7473aa655ffe664c368cd2
Bo Peng <Bo.Peng@usda.gov>
2023-06-23 15:53:16 -0500

    Pull request #34: Story/CIGCPDPH2 1071 split out supplemental entities 5 8
    Merge in NRCSAPPS/pods_csc from story/CIGCPDPH2-1071-split-out-supplemental-entities-5-8 to develop
    
    Squashed commit of the following:
    
    commit e47a76c1f057f84058d07bc1df2f5591db5bb3d8
    Author: Bo Peng <Bo.Peng@usda.gov>
    Date:   Fri Jun 23 14:48:24 2023 -0400
    
        ignore auth.json file
    
    commit 828a0d9ba5c187ddc64f971e9347db9bf2c1a148
    Author: Bo Peng <Bo.Peng@usda.gov>
    Date:   Fri Jun 23 13:28:55 2023 -0400
    
        add views and uid to logs
    
    commit 4217185bf3f09221ce1799ee266d1ac16be3ad14
    Author: Bo Peng <Bo.Peng@usda.gov>
    Date:   Thu Jun 22 15:00:51 2023 -0400
    
        naming error fixed
    
    commit e50fa6c0fac924a72461466b42e3935c0e866004
    Author: Bo Peng <Bo.Peng@usda.gov>
    Date:   Thu Jun 22 14:27:58 2023 -0400
    
        Add data definitions and yml file

---
+++ web/modules/custom/cig_pods_csc/src/Plugin/Log/LogType/ContourBufferStrips.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\cig_pods_csc\Plugin\Log\LogType;
+
+use Drupal\farm_entity\Plugin\Log\LogType\FarmLogType;
+use Drupal\farm_field\FarmFieldFactory;
+
+/**
+ * Provides the Contour Buffer Strips log type.
+ *
+ * @LogType(
+ * id = "contour_buffer_strips",
+ * label = @Translation("ContourBufferStrips"),
+ * )
+ */
+class ContourBufferStrips extends FarmLogType {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+
+    $fields = parent::buildFieldDefinitions();
+
+    $field_info = [
+        'p332_strip_width' => [
+            'type' => 'fraction',
+            'label' => 'Supplemental Data Strip width (feet)',
+            'description' => 'Supplemental Data Strip width (feet)',
+            'required' => FALSE,
+            'multiple' => FALSE,
+        ],
+        'p332_species_category' => [
+            'type' => 'list_string',
+            'label' => 'Supplemental Data 332 Species category',
+            'description' => 'Supplemental 332 Data Species category',
+                'allowed_values' => [
+              'Grasses' => t(string: 'Grasses'),
+              'Forbs' => t(string: 'Forbs'),
+              'Mix' => t(string: 'Mix'),
+            ],
+            'required' => FALSE,
+            'multiple' => FALSE,
+        ],
+        'project_id' => [
+            'type' => 'entity_reference',
+            'label' => 'Project ID',
+            'description' => 'Project ID',
+            'target_type' => 'asset',
+            'target_bundle' => 'project_summary',
+            'required' => TRUE,
+            'multiple' => FALSE,
+        ]
+    ];
+
+    $farmFieldFactory = new FarmFieldFactory();
+
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $farmFieldFactory->bundleFieldDefinition($info)
+        ->setDisplayConfigurable('form', TRUE)
+        ->setDisplayConfigurable('view', TRUE);
+    }
+
+    return $fields;
+
+  }
+
+}
---
+++ web/modules/custom/cig_pods_csc/src/Plugin/Log/LogType/CriticalAreaPlanting.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\cig_pods_csc\Plugin\Log\LogType;
+
+use Drupal\farm_entity\Plugin\Log\LogType\FarmLogType;
+use Drupal\farm_field\FarmFieldFactory;
+
+/**
+ * Provides the Critical Area Planting log type.
+ *
+ * @LogType(
+ * id = "critical_area_planting",
+ * label = @Translation("CriticalAreaPlanting"),
+ * )
+ */
+class CriticalAreaPlanting extends FarmLogType {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+
+    $fields = parent::buildFieldDefinitions();
+
+    $field_info = [
+        'p342_species_category' => [
+            'type' => 'list_string',
+            'label' => 'Supplemental Data 342 Species category',
+            'description' => 'Supplemental Data 342 Species category',
+                'allowed_values' => [
+              'Grass' => t(string: 'Grass'),
+              'Grass legume/forb mix' => t(string: 'Grass legume/forb mix'),
+              'Herbaceous woody mix' => t(string: 'Herbaceous woody mix'),
+              'Perennial or reseeding' => t(string: 'Perennial or reseeding'),
+              'Shrubs ' => t(string: 'Shrubs'),
+              'Trees' => t(string: 'Trees'),
+            ],
+            'required' => FALSE,
+            'multiple' => FALSE,
+        ],
+        'project_id' => [
+            'type' => 'entity_reference',
+            'label' => 'Project ID',
+            'description' => 'Project ID',
+            'target_type' => 'asset',
+            'target_bundle' => 'project_summary',
+            'required' => TRUE,
+            'multiple' => FALSE,
+        ]
+    ];
+
+    $farmFieldFactory = new FarmFieldFactory();
+
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $farmFieldFactory->bundleFieldDefinition($info)
+        ->setDisplayConfigurable('form', TRUE)
+        ->setDisplayConfigurable('view', TRUE);
+    }
+
+    return $fields;
+
+  }
+
+}
