5276541f76c6505780dca72ce8c22967ab1bbffd
Evan Kelley <Evan.Kelley@usda.gov>
2023-06-28 13:03:30 -0500

    Pull request #38: CIGCPDPH2 1072 supplemental data update
    Merge in NRCSAPPS/pods_csc from CIGCPDPH2-1072-supplemental-data-update to develop
    
    Squashed commit of the following:
    
    commit d07f0a3765b0ce74ca4bc2835aa8268e1ec1caf7
    Merge: 5b02b40 55152b2
    Author: ekelley9 <evan.kelley@spatialfront.com>
    Date:   Wed Jun 28 11:29:15 2023 -0600
    
        merged in current dev
    
    commit 5b02b4070f39ff943a869aabdab677fac19e2a10
    Author: ekelley9 <evan.kelley@spatialfront.com>
    Date:   Wed Jun 28 11:24:07 2023 -0600
    
        added all the views for each new log
    
    commit 322940baebb3fc220b51e12e55c8d15e850101aa
    Author: ekelley9 <evan.kelley@spatialfront.com>
    Date:   Tue Jun 27 14:24:54 2023 -0600
    
        adjusted the field value in forest farming log to be the correct field definition
    
    commit 87a79cbf052d0f5db6bd8b9187b0a6f31ebf7a66
    Author: ekelley9 <evan.kelley@spatialfront.com>
    Date:   Tue Jun 27 13:40:29 2023 -0600
    
        removed supplemental data files
    
    commit ba5e5ba86620fa76e46024067dc806c099bf8587
    Author: ekelley9 <evan.kelley@spatialfront.com>
    Date:   Tue Jun 27 13:10:11 2023 -0600
    
        changed the name of field management to feed management to mro inly with the story
    
    commit 60eac614ded278d63d51f12a96b6e605fff2cfe7
    Author: ekelley9 <evan.kelley@spatialfront.com>
    Date:   Tue Jun 27 10:42:02 2023 -0600
    
        added the project refference to each of the new logs, added the uuid's to the config xync yml files for the new logs
    
    commit dc0023e3b1e04b2af9343bf2db5ad3279d7f8fb3
    Author: ekelley9 <evan.kelley@spatialfront.com>
    Date:   Mon Jun 26 13:52:30 2023 -0600
    
        added the new log types

---
+++ web/modules/custom/cig_pods_csc/src/Plugin/Log/LogType/FieldBorder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\cig_pods_csc\Plugin\Log\LogType;
+
+use Drupal\farm_entity\Plugin\Log\LogType\FarmLogType;
+use Drupal\farm_field\FarmFieldFactory;
+
+/**
+ * Provides the Field Border log type.
+ *
+ * @LogType(
+ * id = "field_border",
+ * label = @Translation("Field Border"),
+ * )
+ */
+class FieldBorder extends FarmLogType {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+
+    $fields = parent::buildFieldDefinitions();
+
+    $field_info = [
+      'p386_species_category' => [
+        'type' => 'list_string',
+        'label' => 'Field Border 386 Species category',
+        'description' => 'Field Border 386 Species category',
+		    'allowed_values' => [
+          'Forbs,' => t(string: 'Forbs'),
+          'Grasses' => t(string: 'Grasses'),
+          'Mix' => t(string: 'Mix'),
+          'Shrubs' => t(string: 'Shrubs'),
+        ],
+        'required' => FALSE,
+        'multiple' => FALSE,
+      ],
+      'project_id' => [
+        'type' => 'entity_reference',
+        'label' => 'Project ID',
+        'description' => 'Project ID',
+        'target_type' => 'asset',
+        'target_bundle' => 'project_summary',
+        'required' => TRUE,
+        'multiple' => FALSE,
+      ],
+    ];
+
+    $farmFieldFactory = new FarmFieldFactory();
+
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $farmFieldFactory->bundleFieldDefinition($info)
+        ->setDisplayConfigurable('form', TRUE)
+        ->setDisplayConfigurable('view', TRUE);
+    }
+
+    return $fields;
+
+  }
+
+}
---
+++ web/modules/custom/cig_pods_csc/src/Plugin/Log/LogType/ForestFarming.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\cig_pods_csc\Plugin\Log\LogType;
+
+use Drupal\farm_entity\Plugin\Log\LogType\FarmLogType;
+use Drupal\farm_field\FarmFieldFactory;
+
+/**
+ * Provides the Forest Farming log type.
+ *
+ * @LogType(
+ * id = "forest_farming",
+ * label = @Translation("Forest Farming"),
+ * )
+ */
+class ForestFarming extends FarmLogType {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+
+    $fields = parent::buildFieldDefinitions();
+
+    $field_info = [
+      'p379_land_use_previous_years' => [
+        'type' => 'list_string',
+        'label' => 'Forest Farming Land use in previous years',
+        'description' => 'Forest Farming Land use in previous years',
+		    'allowed_values' => [
+          'Forest,' => t(string: 'Forest'),
+          'Multi-story cropping' => t(string: 'Multi-story cropping'),
+          'Row crops' => t(string: 'Row crops'),
+          'Pasture/grazing land' => t(string: 'Pasture/grazing land'),
+          'Other agroforestry ' => t(string: 'Other agroforestry '),
+        ],
+        'required' => FALSE,
+        'multiple' => FALSE,
+      ],
+      'project_id' => [
+        'type' => 'entity_reference',
+        'label' => 'Project ID',
+        'description' => 'Project ID',
+        'target_type' => 'asset',
+        'target_bundle' => 'project_summary',
+        'required' => TRUE,
+        'multiple' => FALSE,
+      ],
+    ];
+
+    $farmFieldFactory = new FarmFieldFactory();
+
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $farmFieldFactory->bundleFieldDefinition($info)
+        ->setDisplayConfigurable('form', TRUE)
+        ->setDisplayConfigurable('view', TRUE);
+    }
+
+    return $fields;
+
+  }
+
+}
