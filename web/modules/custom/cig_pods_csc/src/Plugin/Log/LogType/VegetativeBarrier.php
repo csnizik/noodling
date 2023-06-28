55152b261fdb9d87c558a1b9bd3b96f160f8bb74
Samuel Alwine <Samuel.Alwine@usda.gov>
2023-06-28 08:04:24 -0500

    Pull request #36: CIGCPDPH2 1078 supplemental entities 30 32
    Merge in NRCSAPPS/pods_csc from CIGCPDPH2-1078-supplemental-entities-30-32 to develop
    
    Squashed commit of the following:
    
    commit da67d88f72dae4936c3f63eb70da8700bee61c75
    Author: USDA\Samuel.Alwine <samuel.alwine@usda.gov>
    Date:   Tue Jun 27 15:40:03 2023 -0400
    
        Adding project ID to views
    
    commit 655c20cea30786eef1c9f3eb4338855963a5ee30
    Author: USDA\Samuel.Alwine <samuel.alwine@usda.gov>
    Date:   Tue Jun 27 15:25:13 2023 -0400
    
        Correcting fields in the php file and views
    
    commit 0e400999294b829468cef736750927084a9aad46
    Author: USDA\Samuel.Alwine <samuel.alwine@usda.gov>
    Date:   Tue Jun 27 14:36:38 2023 -0400
    
        Updating species category to the correct field
    
    commit a89af623ae44729646039d121c68fad875e5c792
    Author: USDA\Samuel.Alwine <samuel.alwine@usda.gov>
    Date:   Tue Jun 27 11:57:04 2023 -0400
    
        Adding views in config/install
    
    commit 1a967d57634181163300cc8b6ddaa02f147977e5
    Merge: f6ba44d 1995e3b
    Author: USDA\Samuel.Alwine <samuel.alwine@usda.gov>
    Date:   Tue Jun 27 11:54:51 2023 -0400
    
        Merging dev into this branch
    
    commit f6ba44d92dc75150d3ce6a627aaebc2f3e5d80e0
    Author: USDA\Samuel.Alwine <samuel.alwine@usda.gov>
    Date:   Mon Jun 26 15:48:37 2023 -0400
    
        Adding last field, fixing formatting
    
    commit 4debeead1c664920df88021c1f05f7f6a4b3acff
    Author: USDA\Samuel.Alwine <samuel.alwine@usda.gov>
    Date:   Mon Jun 26 14:43:10 2023 -0400
    
        Removing a comma
    
    commit daa20b5e5401b4697550d49fe3322a9c0beb9746
    Author: USDA\Samuel.Alwine <samuel.alwine@usda.gov>
    Date:   Mon Jun 26 12:33:54 2023 -0400
    
        Editing field for vegetative barrier
    
    commit 11ed78b99527916c61edb171a2ecb041a15ecaa0
    Author: USDA\Samuel.Alwine <samuel.alwine@usda.gov>
    Date:   Fri Jun 23 14:02:53 2023 -0400
    
        Another commit before a repods
    
    commit 8e3f375e0661100452bd88b4dd6ac26e322fa43f
    Author: USDA\Samuel.Alwine <samuel.alwine@usda.gov>
    Date:   Thu Jun 22 15:36:52 2023 -0400
    
        Initial commit for a repods

---
+++ web/modules/custom/cig_pods_csc/src/Plugin/Log/LogType/VegetativeBarrier.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\cig_pods_csc\Plugin\Log\LogType;
+
+use Drupal\farm_entity\Plugin\Log\LogType\FarmLogType;
+use Drupal\farm_field\FarmFieldFactory;
+
+/**
+ * Provides the Vegetative Barrier log type.
+ *
+ * @LogType(
+ * id = "vegetative_barrier",
+ * label = @Translation("VegetativeBarrier"),
+ * )
+ */
+class VegetativeBarrier extends FarmLogType {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+
+    $fields = parent::buildFieldDefinitions();
+
+    $field_info = [
+        'project_id' => [
+          'type' => 'entity_reference',
+          'label' => 'Project ID',
+          'description' => 'Project ID',
+          'target_type' => 'asset',
+          'target_bundle' => 'project_summary',
+          'required' => TRUE,
+          'multiple' => FALSE,
+        ],
+        'p601_species_category' => [
+          'type' => 'list_string',
+          'label' => 'Supplemental Data 601 Species Category',
+          'description' => 'Supplemental Data 601 Species Category',
+          'allowed_values' => [
+            'Grasses' => t(string: 'Grasses'),
+            'Grass forb mix' => t(string: 'Grass forb mix'),
+            'Grass legume mix' => t(string: 'Grass legume mix'),
+          ],
+          'required' => FALSE,
+          'multiple' => FALSE,
+        ],
+        'p601_barrier_width' => [
+          'type' => 'fraction',
+          'label' => 'Supplemental Data Barrier Width (feet)',
+          'description' => 'Supplemental Data Barrier Width (feet)',
+          'required' => FALSE,
+          'multiple' => FALSE,
+        ],
+    ];
+
+    $farmFieldFactory = new FarmFieldFactory();
+
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $farmFieldFactory->bundleFieldDefinition($info)
+        ->setDisplayConfigurable('form', TRUE)
+        ->setDisplayConfigurable('view', TRUE);
+    }
+
+    return $fields;
+
+  }
+
+}
---
+++ web/modules/custom/cig_pods_csc/src/Plugin/Log/LogType/WasteStorageFacility.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\cig_pods_csc\Plugin\Log\LogType;
+
+use Drupal\farm_entity\Plugin\Log\LogType\FarmLogType;
+use Drupal\farm_field\FarmFieldFactory;
+
+/**
+ * Provides the Waste Storage Facility log type.
+ *
+ * @LogType(
+ * id = "waste_storage_facility",
+ * label = @Translation("WasteStorageFacility"),
+ * )
+ */
+class WasteStorageFacility extends FarmLogType {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+
+    $fields = parent::buildFieldDefinitions();
+
+    $field_info = [
+      'project_id' => [
+        'type' => 'entity_reference',
+        'label' => 'Project ID',
+        'description' => 'Project ID',
+        'target_type' => 'asset',
+        'target_bundle' => 'project_summary',
+        'required' => TRUE,
+        'multiple' => FALSE,
+      ],
+      'p313_prior_waste_storage_system' => [
+        'type' => 'entity_reference',
+        'label' => 'Supplemental Data Waste storage system prior to installing your waste storage facility',
+        'description' => 'Supplemental Data Waste storage system prior to installing your waste storage facility',
+          'target_type' => 'taxonomy_term',
+          'target_bundle' => 'waste_storage_system',
+        'required' => FALSE,
+        'multiple' => FALSE,
+        ],
+    ];
+
+    $farmFieldFactory = new FarmFieldFactory();
+
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $farmFieldFactory->bundleFieldDefinition($info)
+        ->setDisplayConfigurable('form', TRUE)
+        ->setDisplayConfigurable('view', TRUE);
+    }
+
+    return $fields;
+
+  }
+
+}
