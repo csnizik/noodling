fe26841d3bcea9c39f8b38acc610532c68b950c5
Evan Kelley <Evan.Kelley@usda.gov>
2023-11-15 14:52:33 -0600

    Pull request #105: removed duplicate method
    Merge in NRCSAPPS/pods from CIGCPDPH2-csv-import-continuation to develop
    
    Squashed commit of the following:
    
    commit fb56f6e92e7ecc3703be490b2ba7b77490ef2fe9
    Author: ekelley9 <evan.kelley@spatialfront.com>
    Date:   Wed Nov 15 10:55:44 2023 -0700
    
        removed duplicate method

--- web/modules/custom/cig_pods/src/Controller/PopupController.php
+++ web/modules/custom/cig_pods/src/Controller/PopupController.php
@@ -37,21 +37,4 @@ class PopupController extends ControllerBase {
     return $response;
   }
 
-  public function openDeleteConfirmModal($asset_id) {
-
-    $asset_type = \Drupal::entityTypeManager()->getStorage('asset')->load($asset_id)->type->entity->label();
-
-    $response = new AjaxResponse();
-    
-    $modal_form = $this->formBuilder()->getForm('Drupal\cig_pods\Form\ConfirmDeleteModalForm');
-    $options = [
-      'width' => '50%',
-      'classes' => 'confirm-modal-popup',
-      'dialogClass' => 'confirm-dialog-popup',
-    ];
-    $response->addCommand(new OpenModalDialogCommand('Are you sure you want to delete '.$asset_type.'?', $modal_form, $options));
-    return $response;
-  }
-
-
 }
